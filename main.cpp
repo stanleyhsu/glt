fd1d23ee058e3172d4bacce7bc1323288e8d9b4c
Xu Bingcan <bingcanxu@gmail.com>
2020-09-23 22:39:54 +0800

    make macosx happy...fix its segfault

--- main.cpp
+++ main.cpp
@@ -5,34 +5,47 @@
 
 using namespace std;
 
-void init(GLFWwindow* w){
-
+void init(GLFWwindow *w)
+{
 }
 
-void display(GLFWwindow *w, double currentTime) {
+void display(GLFWwindow *w, double currentTime)
+{
   glClearColor(1.0, 0.0, 0.0, 1.0);
   glClear(GL_COLOR_BUFFER_BIT);
 }
 
-int main() {
-  if (!glfwInit()) {return(-1);}
-  
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+int main()
+{
+  if (!glfwInit())
+  {
+    return (-1);
+  }
+
+  glfwWindowHint(GLFW_SAMPLES, 4);               // 4x antialiasing
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3); // We want OpenGL 3.3
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  GLFWwindow * window = glfwCreateWindow(600, 600, "Hello OpenGL", NULL, NULL);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);           // To make MacOS happy; should not be needed
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //We don't want the old OpenGL
+
+  GLFWwindow *window = glfwCreateWindow(600, 600, "Hello OpenGL", NULL, NULL);
   glfwMakeContextCurrent(window);
 
-  if (glewInit()!=GLEW_OK){return(-1);}
+  if (glewInit() != GLEW_OK)
+  {
+    return (-1);
+  }
   glfwSwapInterval(1);
 
   init(window);
 
-  while(!glfwWindowShouldClose(window)) {
+  while (!glfwWindowShouldClose(window))
+  {
     display(window, 0);
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
   glfwDestroyWindow(window);
   glfwTerminate();
-  return(0);
+  return (0);
 }
